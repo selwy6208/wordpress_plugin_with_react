7a93ff2b17096db0b2c32fc8ce286ddc633b3f76
LightStar1018 <genesis01018@gmail.com>
2023-05-16 00:00:36 +0900

    Start  wp_plugin_with_react

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '7909ae9f43f783cccf70');
---
+++ src/TestComponent.js
@@ -0,0 +1,10 @@
+import React from "react"
+function TestComponent() {
+    return (
+        <div className="test">
+            <h1>Hello</h1>
+            <p>ooooooooo</p>
+        </div>
+    )
+}
+export default TestComponent
